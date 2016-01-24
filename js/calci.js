095d24c66195b8314c4cbb13ce5d08512f7d2eea
rajkumar <rajkumar.gootam@gmail.com>
2016-01-24 23:20:55 +0530

    Handle operator corner cases

--- js/calci.js
+++ js/calci.js
@@ -5,19 +5,39 @@ function handleInput(key) {
 function previewContent(){
   return $('#preview').html();
 }
+
+function deleteLastChar() {
+  var preview  = previewContent();
+  var newPreview = preview.slice(0, -1);
+  $('#preview').html(newPreview);
+}
+
+function KeyIsOperator(key) {
+  return (["+","-","*","/"].indexOf(key) != -1);
+}
+
 $(document).ready(function() {
   $('.key').click(function() {
-    var key = $(this).text();
+    var key = $(this).html();
     if(key == "0"){
      if (previewContent() != "0") {
        handleInput(key);
       }
     }else if (key == "DEL") {
-      var preview  = previewContent();
-      var newPreview = preview.slice(0, -1);
-      $('#preview').html(newPreview);
+      deleteLastChar();
+    } else if (KeyIsOperator(key)) {
+      // Sucessive operators
+      var lastChar = previewContent().slice(-1);
+      if(KeyIsOperator(lastChar)) {
+        deleteLastChar();
+      }
+      if((previewContent() != "") || (key == "-")) {
+      handleInput(key);  
+      }
+
+      //First key operators
     } else {
-    handleInput(key)
+    handleInput(key);
     }
   });
 });
