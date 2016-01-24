2ed3355ff10c0fdf24101dafda32656531a4a2ce
rajkumar <rajkumar.gootam@gmail.com>
2016-01-24 15:02:36 +0530

    Clicking on delete should remove the last characters from the preview

--- js/calci.js
+++ js/calci.js
@@ -1,17 +1,21 @@
 function handleInput(key) {
    $('#preview').append(key);
-
 }
 
-
-
+function previewContent(){
+  return $('#preview').html();
+}
 $(document).ready(function() {
   $('.key').click(function() {
     var key = $(this).text();
     if(key == "0"){
-     if ($('#preview').html() != "0") {
-       handleInput(key)
+     if (previewContent() != "0") {
+       handleInput(key);
       }
+    }else if (key == "DEL") {
+      var preview  = previewContent();
+      var newPreview = preview.slice(0, -1);
+      $('#preview').html(newPreview);
     } else {
     handleInput(key)
     }
